44177d7acf92ee8bcf58e35a66047ff128e67850
SeungJae <stopj1123@gmail.com>
2021-03-07 15:27:53 +0900

    Added searching to user profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
+from profiles_api import permissions
 
 
 class HelloApiView(APIView):
@@ -100,3 +103,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)  # Intended to be tuple, not a single item
+    search_fields = ('name', 'email',)
+    
